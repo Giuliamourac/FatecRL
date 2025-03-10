c0e98b48de98712cece0cdbacc3b981d1777a93a
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-03-10 08:49:57 -0300

    finaliza programa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Fatec Rubens Lara (Baixada Santista).");
+
+        }
+
+    }
+
+}
